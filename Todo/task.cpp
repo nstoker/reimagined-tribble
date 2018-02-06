245f4a7cf5b184654a7dcbb728c34f9a3ce0269a
Neil Stoker <neil.stoker@gmail.com>
2018-02-06 22:10:34 +0000

    Adjust to two lines

--- Todo/task.cpp
+++ Todo/task.cpp
@@ -8,7 +8,8 @@ Task::Task(const QString& name, QWidget *parent) :
 {
     ui->setupUi(this);
     setName(name);
-    connect(ui->editButton, &QPushButton::clicked, this, &Task::rename);
+    connect(ui->editButton, &QPushButton::clicked,
+            this, &Task::rename);
 }
 
 Task::~Task()
