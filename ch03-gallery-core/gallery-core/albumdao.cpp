67b100bb03803671a0c06138e9fce8fd3b46304e
Neil Stoker <neil.stoker@koiostechnology.co.uk>
2018-02-09 15:43:09 +0000

    Fix sql bugs

--- ch03-gallery-core/gallery-core/albumdao.cpp
+++ ch03-gallery-core/gallery-core/albumdao.cpp
@@ -21,7 +21,7 @@ void AlbumDao::init() const
 {
     if(!mDatabase.tables().contains("albums")){
         QSqlQuery query(mDatabase);
-        query.exec("CREATE TABLE albums (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT");
+        query.exec("CREATE TABLE albums (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT)");
         DatabaseManager::debugQuery(query);
     }
 }
--- ch03-gallery-core/gallery-core/picturedao.cpp
+++ ch03-gallery-core/gallery-core/picturedao.cpp
@@ -24,7 +24,7 @@ void PictureDao::init() const
     if(!mDatabase.tables().contains("pictures")) {
         QSqlQuery query(mDatabase);
         query.exec(QString("CREATE TABLE pictures ("
-                           "id INTEGER PRIMARY KEY AUTOINCREMENT"
+                           "id INTEGER PRIMARY KEY AUTOINCREMENT, "
                            "album_id INTEGER, "
                            "url TEXT)"
                            ));
